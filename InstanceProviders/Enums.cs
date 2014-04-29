61aab87f4e455b71c0db57a85df8fba567e30022
Hari <hariharanpalanee@gmail.com>
2014-04-29 14:32:42 +0530

    Create Enums.cs

---
+++ InstanceProviders/Enums.cs
@@ -0,0 +1,21 @@
+public enum TypeLifeStyle
+{
+    Undefined = 0,
+    Transient,
+    Singleton,
+    PerWebRequest
+}
+
+public enum InterfaceType
+{
+    FirstInterface = 0,
+    All,
+    None
+}
+    
+public enum ContainerType
+{
+    Windsor,
+    StructureMap,
+    NInject
+}    
